16b325e528b6d83a1524ef0f7f982069223ce2c2
linyoujie <linyoujie@hotmail.com>
2020-04-09 11:55:04 -0400

    init

---
+++ Home.js
@@ -0,0 +1,53 @@
+import React, { PureComponent } from "react";
+import { AppRegistry, StyleSheet, StatusBar } from "react-native";
+import { GameEngine } from "react-native-game-engine";
+import { Finger } from "./renderers";
+import { MoveFinger } from "./systems"
+import Images from './Images';
+import Cat from './Cat'
+import Matter from 'matter-js';
+
+
+export default class Home extends PureComponent {
+  constructor() {
+    super();
+    this.entities = this.setupWorld();
+  }
+
+
+  setupWorld = () =>{
+    let engine = Matter.Engine.create ({ enableSleeping: false});
+    let world = engine.world;
+    world.gravity.y = 0
+    
+    let cat = Matter.Bodies.rectangle(300, 200,100,120)
+
+    Matter.World.add(world, [Cat])
+
+    return { Cat:{body: cat, renderer:Cat}}
+
+}
+
+  render() {
+    return (
+     
+
+      <GameEngine
+        style={styles.container}
+        systems={[MoveFinger]}
+        entities={this.entities}>
+
+        <StatusBar hidden={true} />
+
+      </GameEngine>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFF"
+  }
+});
+
---
+++ Images.js
@@ -0,0 +1,16 @@
+export default Images = {
+    angry_static : require('./assets/cat/angry_static.png'),
+    drag : require('./assets/cat/drag.gif'),
+    hungry : require('./assets/cat/hungry.gif'),
+    idle : require('./assets/cat/idle.gif'),
+    laydown_static : require('./assets/cat/laydown_static.png'),
+    nice_static : require('./assets/cat/nice_static.png'),
+    ready_static : require('./assets/cat/ready_static.png'),
+    run : require('./assets/cat/run.gif'),
+    stood : require('./assets/cat/stood.gif'),
+    tear_static : require('./assets/cat/tear_static.png'),
+    walk_staitc : require('./assets/cat/walk_staitc.png'),
+
+
+
+}
